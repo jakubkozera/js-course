0b2949cdf1923bc76f3a1a63a36e579bc76d7b71
Jakub Kozera <kozerakuba@gmail.com>
2023-10-21 09:04:11 +0200

    1.4 Typy danych

--- file.js
+++ file.js
@@ -2,11 +2,16 @@ console.log("Hello world, ")
 
 console.log("from Visual studio code")
 
+let boolean1 = true
+let boolean2 = false
+
+let sampleNumber = 32.2323
+console.log(sampleNumber)
+
 {
     let userName = "test2"
 
-    console.log(test)
+    console.log(userName)
 }
-console.log(test)
 
 
