8804dd641ba6d66cca56eb04889b7834903bf121
Jakub Kozera <kozerakuba@gmail.com>
2023-10-23 07:44:31 +0200

    Rozwiązanie

--- file.js
+++ file.js
@@ -1,32 +1,19 @@
-function helloName(name) {
+// utwórz funkcje, która przyjmuje dwa parametry: imie i nazwisko, a w rezultacie zwróci jedną wartość 'imie nazwisko'
+// zdefiniuj ją za pomocą słowa kluczowego function, zdefiniuj jako zmienną z funkcją anonimową oraz funkcje strzałkową (3 warianty)
+// całość przetestuj
 
-    let result = "hello, " + name
-    return result
-
+function getDisplayName1(firstName, lastName) {
+    return firstName + " " + lastName
 }
 
-const variableFunction = function(name) {
-
-    let result = "hello, " + name
-    return result
+const getDisplayName2 = function(firstName, lastName) {
+    return firstName + " " + lastName
 }
 
-const arrowFunction = (name) => {
-
-    let result = "hello, " + name
-    return result
+const getDisplayName3 = (firstName, lastName) => {
+    return firstName + " " + lastName
 }
 
-
-let testName = "test"
-
-const result1 = variableFunction("Kuba")
-console.log(result1)
-
-const result2 = helloName(testName)
-console.log(result2)
-
-const resultFromArrowFunction = arrowFunction("arrow")
-console.log(resultFromArrowFunction)
-
-const result3 = helloName()
+console.log(getDisplayName1("Test", "Display1"))
+console.log(getDisplayName2("Test", "Display2"))
+console.log(getDisplayName3("Test", "Display3"))
