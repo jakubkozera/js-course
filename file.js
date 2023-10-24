65583f441529d3e71aed86ab08bfeca84656f547
Jakub Kozera <kozerakuba@gmail.com>
2023-10-24 07:20:36 +0200

    1.6 Typ number  - zadanie

--- file.js
+++ file.js
@@ -1,40 +1,13 @@
-// typ number
+// utwórz funkcje, która przyjmuje dwa parametry: a, b
+// rezultatem tej funkcji powinna być wartość typu boolean
+// określająca czy liczba a jest podzielna przez b
 
-let a = 10
-let b = 5
 
-let sum = a + b
-let difference = a - b
-let product = a * b
-let quotient = a / b
+function isDivisibleBy(a, b) {
+    return a % b === 0
+}
 
-console.log(sum)
-console.log(difference)
-console.log(product)
-console.log(quotient)
 
-let evenNumber = 4
-let oddNumber = 7
-
-console.log(evenNumber % 2)
-console.log(oddNumber % 2)
-
-let square = a ** 2
-console.log(square)
-
-let textualNumber = "123"
-console.log(parseInt(textualNumber) + 2)
-
-console.log(a === "10")
-console.log(a == "10")
-console.log(a !== b)
-console.log(a > b)
-console.log(a < b)
-console.log(a >= b)
-console.log(a <= b)
-
-// Math
-
-console.log(Math.floor(5.9))
-console.log(Math.round(5.6))
-console.log(Math.abs(-5.6))
+console.log(isDivisibleBy(6,3))
+console.log(isDivisibleBy(6,4))
+console.log(isDivisibleBy(5,2))
