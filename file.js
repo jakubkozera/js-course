9ff27f23abd5522e92b4e3123d3a8eebd22a23c7
Jakub Kozera <kozerakuba@gmail.com>
2023-10-29 15:43:42 +0100

    2.2 Metoda foreach

--- file.js
+++ file.js
@@ -1,18 +1,9 @@
-// wykorzystując funkcje foreach, stwórz funkcje, która zwróci sume wszystkcich elementów tablicy
-// oraz drugą funkcje, która zwróci średnią wartość elementów tablicy, zaokrągloną w dól (Math.floor)
-
+// tablice - foreach
 const t = [23, 12, 5, 9, 45]
 
-function sum(arr) {
-    let sum = 0
-    arr.forEach((element) => sum += element)
+t.forEach((element, index, arr) => {
+    console.log("Index: " + index + ", wartość: " + element)
+    arr[index] =  element ** 2
+})
 
-    return sum
-}
-
-function average(arr) {
-    return Math.floor(sum(arr) / arr.length)
-}
-
-console.log(sum(t))
-console.log(average(t))
+console.log(t)
