a1cc0954da4c5c2cae0a5f45b2c2297c087a9fe2
Jakub Kozera <kozerakuba@gmail.com>
2023-11-05 07:08:26 +0100

    4.2 Operator trójargumentowy

--- file.js
+++ file.js
@@ -1,23 +1,14 @@
-// instrukcja warunkowa if-else - zadanie
-// napisz funkcje, która dla argumentu określającego wartość zamówienia obliczy rabat procentowy, z założeniami;
-// od kwoty 100 zł - rabat 3%
-// od kwoty 200 zł - rabat 5%
-// całość przetestuj 
+// operator trójargumentowy
 
-
-function calculateDiscount(amount) {
-    if(amount >= 200) {
-        return 0.05
-    } else if(amount >= 100) {
-        return 0.03
-    } else {
-        return 0
-    }
+function checkEvenOdd(number) {
+    return number === 0 ? "Zero" 
+                : number % 2 === 0 ? "Even" : "Odd"
 }
+// 1 warunek - boolean
+// 2 wartość zwrócona z operatora, jeżeli warunek jest true
+// 3 wartość zwrócona z operatora, jeżeli warunek jest false
+// warunek ? "rezultat jeśli true" : "rezultat jeśli false"
 
-console.log("300, rabat: ", calculateDiscount(300))
-console.log("200, rabat: ",calculateDiscount(200))
-console.log("100, rabat: ",calculateDiscount(100))
-console.log("150, rabat: ",calculateDiscount(150))
-console.log("50, rabat: ",calculateDiscount(50))
-console.log("5, rabat: ",calculateDiscount(5))
+
+const numberStatus = checkEvenOdd(0);
+console.log(numberStatus); // Wyświetli: "Odd"
