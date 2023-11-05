4cfdecbd85e4f524643b03687f21c8ff58513961
Jakub Kozera <kozerakuba@gmail.com>
2023-11-05 09:16:19 +0100

    4.3 Instrukcja switch - zadanie

--- file.js
+++ file.js
@@ -1,26 +1,34 @@
-// instrukcja switch
+// instrukcja switch - zadanie
+// Napisz funkcje, która dla oceny od 1 do 5
+// Zwróci jej tekstową reprezentacje
+// stopień bardzo dobry – 5,
+// stopień dobry – 4,
+// stopień dostateczny – 3,
+// stopień dopuszczający – 2,
+// stopień niedostateczny – 1
+// dla "nieprawidłowej" oceny, zwróci odpowiednia informacje
 
-const date = new Date()
-
-const day = date.getDay()
-
-console.log(day)
-
-function getDayName(day) {
-    switch (day) {
+function getGrade(grade) {
+    switch(grade) {
         case 1:
-            return "Poniedziałek"
+            return "niedostateczny"
         case 2:
-            return "Wtorek"
+            return "dopuszczający"
         case 3:
-            return "Środa"
+            return "dostateczny"
         case 4:
-            return "Czwartek"
+            return "dobry"
         case 5:
-            return "Piątek"
+            return "bardzo dobry"
         default:
-            return "Weekend"
+            return "nieprawidłowa ocena"
     }
 }
 
-console.log(getDayName(4))
+console.log(getGrade(1))
+console.log(getGrade(2))
+console.log(getGrade(3))
+console.log(getGrade(4))
+console.log(getGrade(5))
+console.log(getGrade(235))
+console.log(getGrade(51))
