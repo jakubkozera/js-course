67b394a4d7f24b156f5af8e09d6665d43800e48c
Jakub Kozera <kozerakuba@gmail.com>
2023-11-06 08:10:15 +0100

    4.6 Pętla for-of - zadanie

--- file.js
+++ file.js
@@ -1,20 +1,24 @@
 // pętla for-of
+// utwórz metodę imitującą metodę some, która przyjmuje 2 argumenty
+// 1 - dowolną tablicę t
+// 2 - predykatę (funkcję zwracającą true/false)
+// rezultatem z tej metody powinna być wartość boolean, określający czy w danej tablicy jest jaki kolwiek element
+// który spełnia daną predykatę
+// przykładowo:
 
-const fruits = ["apple", "banana", "orange"]
+const numbers = [1, 2, 3, 6, 90] 
+console.log(numbers.some(n => n % 2 === 0))
+console.log(numbers.some(n => n < 0))
 
-for (const fruit of fruits) {
-  if(fruit.length > 5) {
-    continue;
-  } 
-  console.log(fruit)
+function some(arr, predicate) {
+
+  for (const element of arr) {
+    if(predicate(element)) {
+      return true
+    }
+  }
+  return false
 }
 
-const numbers = [1,2,3,4, 90]
-
-let sum = 0;
-
-for (const value of numbers) {
-  sum += value
-}
-
-console.log(sum)
+console.log(some(numbers, n => n % 2 === 0))
+console.log(some(numbers, n => n < 0))
