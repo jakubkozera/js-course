fc4a90eceec4547a76189ce1daeb9ce2b03cce3a
Jakub Kozera <kozerakuba@gmail.com>
2023-11-07 08:26:19 +0100

    4.8 Pętla while i do-while

--- file.js
+++ file.js
@@ -1,18 +1,21 @@
-// pętla for-in - zadanie
-// dla poniższego obiektu people,który przechowuje imię osoby oraz wyniki z testów pod postacią tablicy
-// napisz kod, który wypisze do konsoli, które osoby miały conajmniej 1 wynik, który przekraczał próg 70 pkt
-const people = {
-  "Adam": [23, 55, 53],
-  "John": [53, 59, 73],
-  "Pete": [3, 5, 2],
-  "Tomasz": [64, 75, 56],
-}
+// pętla while i do-while
 
-
-for(const person in people) {
-  const results = people[person] // [23, 55, 53]
-  const hasHighScore = results.some(r => r > 70)
-  if(hasHighScore) {
-    console.log(`${person} has scored a high score`)
+let i = 0
+while (i < 5) {
+  if(i === 3) {
+    i++;
+    continue
   }
+  console.log("i",i)
+  i++;
 }
+
+let j = 0
+
+do {
+  if(j === 3) {
+    break
+  }
+  console.log("j", j)
+  j++;
+} while (j < 5)
