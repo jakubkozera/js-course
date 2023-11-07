9c0480622e3b51b77a525a5777206fb0c9691035
Jakub Kozera <kozerakuba@gmail.com>
2023-11-07 08:56:26 +0100

    4.8 Pętla while - zadanie

--- file.js
+++ file.js
@@ -1,21 +1,29 @@
-// pętla while i do-while
+// pętla while - zadanie
+// utwórz metodę 'all', imitującą metodę 'every', dla tablic - wykorzystując pętle while
+// powinna przyjąć ona 2 argumenty:
+// 1 - dowolna tablica
+// 2 - dowolna predykata
+// rezultatem z tej metody jest wartość boolean = true, jeżeli wszystkie elementy z tablicy spełniają predykatę
+// w przeciwnym wypadku rezultatem jest wartość false
 
-let i = 0
-while (i < 5) {
-  if(i === 3) {
-    i++;
-    continue
+const t1 = [5, 10, 15, 90]
+
+console.log(t1.every(t => t > 0))
+console.log(t1.every(t => t % 2 == 0))
+
+function all(array, predicate) {
+  let i = 0
+  while(i < array.length) {
+    const element = array[i]
+
+    if(!predicate(element)) {
+      return false
+    }
+    i++
   }
-  console.log("i",i)
-  i++;
+
+  return true
 }
 
-let j = 0
-
-do {
-  if(j === 3) {
-    break
-  }
-  console.log("j", j)
-  j++;
-} while (j < 5)
+console.log(all(t1, t => t > 0))
+console.log(all(t1, t => t % 2 === 0))
