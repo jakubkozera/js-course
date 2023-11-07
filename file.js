8a904e8c48d05956759ed6fad2ca8f439ab2d9f5
Jakub Kozera <kozerakuba@gmail.com>
2023-11-07 07:24:15 +0100

    4.7 Pętla for-in

--- file.js
+++ file.js
@@ -1,24 +1,27 @@
-// pętla for-of
-// utwórz metodę imitującą metodę some, która przyjmuje 2 argumenty
-// 1 - dowolną tablicę t
-// 2 - predykatę (funkcję zwracającą true/false)
-// rezultatem z tej metody powinna być wartość boolean, określający czy w danej tablicy jest jaki kolwiek element
-// który spełnia daną predykatę
-// przykładowo:
+// pętla for-in
 
-const numbers = [1, 2, 3, 6, 90] 
-console.log(numbers.some(n => n % 2 === 0))
-console.log(numbers.some(n => n < 0))
-
-function some(arr, predicate) {
-
-  for (const element of arr) {
-    if(predicate(element)) {
-      return true
-    }
-  }
-  return false
+const isoCountryCodes = {
+  "US" : "United States",
+  "CA": "Canada",
+  "PL": "Poland",
+  "FR": "France",
+  "DE": "Germany"
 }
 
-console.log(some(numbers, n => n % 2 === 0))
-console.log(some(numbers, n => n < 0))
+// const keys = Object.keys(isoCountryCodes)
+// for (const key of keys) {
+//   const countryName = isoCountryCodes[key]
+//   console.log(`Country code: ${key} , country name: ${countryName}`)
+// }
+
+for(const isoCode in isoCountryCodes) {
+    const countryName = isoCountryCodes[isoCode]
+    if(countryName === "Canada") {
+      continue
+    }
+    
+    console.log(`Country code: ${isoCode}, country name: ${countryName}`)
+    if(countryName === "Poland") {
+      break
+    }
+}
