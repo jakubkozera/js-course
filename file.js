d0e52bfba9e0f6bb0a8357f4c80b31defd5c543e
Jakub Kozera <kozerakuba@gmail.com>
2023-11-13 08:05:00 +0100

    1.10 Debuggowanie kodu

--- file.js
+++ file.js
@@ -1,21 +1,30 @@
-// interpolacja string
+// debuggowanie programu
+function displayUser(firstName, lastName, age) {
+    // użycie operatora +
 
-// ${}
+    const message1 = "Hello, my name is " + firstName + " " + lastName + " I'm " + age
+    console.log(message1)
+    // metoda contact
 
-const firstName = "John"
+    const message2 = "Hello, my name is ".concat(firstName, lastName, " I'm ", age)
+    console.log(message2)
+
+}
+
+let firstName = "John"
 const lastName = "Doe"
 const age = 30
 
-// użycie operatora +
-
-const message1 = "Hello, my name is " + firstName + " " + lastName + " I'm " + age
-console.log(message1)
-// metoda contact
-
-const message2 = "Hello, my name is ".concat(firstName, " ", lastName, " I'm ", age)
-console.log(message2)
 
 // interpolacja string
 
 const message3 = `Hello, my name is ${firstName} ${lastName} I'm ${age}`
 console.log(message3)
+
+firstName = "NotJohn"
+
+displayUser(firstName, lastName, age)
+
+displayUser("Paul", "Walker", 35)
+
+console.log('end')
