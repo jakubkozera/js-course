0f1c9e1fc91f4e34a7ae5aa468b4e59023d835a3
Jakub Kozera <kozerakuba@gmail.com>
2023-11-14 07:52:41 +0100

    5.2 Prosta interakcja z użytkownikiem

--- file.js
+++ file.js
@@ -1,1 +1,15 @@
 console.log("Hello from file.js")
+
+alert("Witaj")
+
+const userName = prompt("Jak masz na imię?")
+
+alert(`Cześć ${userName}, miło Cie widzieć`)
+
+const confirmed = confirm("Czy chcesz wygrać darmowego iPhone?")
+
+if(confirmed) {
+    alert("Tak myślałem..")
+} else {
+    alert("Ups..")
+}
