4b5cf5b2e17fb8591bc7ad58828f47b0b8e26338
Jakub Kozera <kozerakuba@gmail.com>
2023-11-14 08:03:55 +0100

    5.2 Prosta interakcja z użytkownikiem - zadanie

--- file.js
+++ file.js
@@ -1,15 +1,11 @@
-console.log("Hello from file.js")
+// Kalkulator BMI:
+// Poproś użytkownika o podanie swojej wagi i wzrostu za pomocą prompt, 
+// a następnie oblicz jego BMI i pokaż wynik za pomocą alert. 
+// Wzór BMI to masa ciała (kg) / (wzrost (m))^2.
 
-alert("Witaj")
+const weight = prompt("Podaj swoją wagę (kg):")
+const height = prompt("Podaj swój wzrost (m)")
 
-const userName = prompt("Jak masz na imię?")
+const bmi = weight / (height * height)
 
-alert(`Cześć ${userName}, miło Cie widzieć`)
-
-const confirmed = confirm("Czy chcesz wygrać darmowego iPhone?")
-
-if(confirmed) {
-    alert("Tak myślałem..")
-} else {
-    alert("Ups..")
-}
+alert("Twój BMI, wynosi: " + bmi.toFixed(2))
