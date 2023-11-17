ea04aa0ab6b81a27fc717d9e8b57ab9565c23edf
Jakub Kozera <kozerakuba@gmail.com>
2023-11-17 08:46:21 +0100

    5.4 Pobieranie elementów DOM

--- file.js
+++ file.js
@@ -1,15 +1,43 @@
 // interakcja przez zdarzenia
 
 function start() {
-    alert("Witaj")
-    const userName = prompt("Jak masz na imie?")
-    alert(`Cześć ${userName}, miło Cie widzieć`)
-    const correctResult = confirm(`Czy chcesz otrzymać darmowego iPhone?`)
-    if(correctResult) {
-        alert("Tak myślałem..")
-    } else {
-        alert("Ups")
+    // pobieranie elementów przez nazwę klasy
+    const alerts = document.getElementsByClassName("alert")
+    for (let i = 0; i < alerts.length; i++) {
+        const element = alerts[i];
+        console.log(element.innerText)
     }
+
+    // pobieranie na podstawie id
+    const input1 = document.getElementById("input1")
+    console.log(input1.value)
+
+    // Pobieranie wszystkich elementów o danej nazwie
+    const descriptions = document.getElementsByName("description")
+    for (let i = 0; i < descriptions.length; i++) {
+        const element = descriptions[i];
+        console.log(element.innerText)
+    }
+
+    // pobieranie wartości po tagu
+    const labels = document.getElementsByTagName("label")
+    for (let i = 0; i < labels.length; i++) {
+        console.log(labels[i].innerText);
+    }
+
+    // pobieranie na podstawie selektora
+    const firstInput = document.querySelector("#input1")
+    console.log(firstInput.value)
+
+    // pobieranie wielu elementów na podstawie selektora
+    console.log("pobieranie wielu elementów na podstawie selektora")
+    const allInputs = document.querySelectorAll("input")
+    for (let i = 0; i < allInputs.length; i++) {
+        const element = allInputs[i];
+        console.log(element.value)
+        
+    }
+
 }
 
 function onInputChange(event) {
