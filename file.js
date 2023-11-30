223a815687de82510b2c9eac30ce120422b20c55
Jakub Kozera <kozerakuba@gmail.com>
2023-11-30 07:56:40 +0100

    6.3  Storage przeglądarki - zadanie z rozwiązaniem

--- file.js
+++ file.js
@@ -10,3 +10,42 @@
 // Dla wygody użytkownika, istnieje przycisk "Wyczyść historię", który usuwa całą historię wyszukiwań.
 
 
+const recentSearchesKey = "recentSearches"
+const getRecentSearches = () => JSON.parse(localStorage.getItem(recentSearchesKey)) || []
+
+function search() {
+   const searchInput = document.getElementById("searchInput")
+   const searchTerm = searchInput.value.trim()
+   if(searchTerm === "") {
+      alert("Wprowadź frazę do wyszukania")
+      return
+   }
+
+   let recentSearch = getRecentSearches()
+
+   recentSearch.unshift(searchTerm)
+   localStorage.setItem(recentSearchesKey, JSON.stringify(recentSearch))
+   searchInput.value = ''
+
+   displayRecentSearched(recentSearch)
+}
+
+function clearHistory() {
+   localStorage.removeItem(recentSearchesKey)
+   displayRecentSearched([])
+}
+
+function displayRecentSearched(searches) {
+   const recentSearchesList = document.getElementById("recentSearches")
+   recentSearchesList.innerHTML = ''
+
+   searches.forEach(searchTerm => {
+      const listItem = document.createElement("li")
+      listItem.classList.add("list-group-item")
+      listItem.textContent = searchTerm
+      recentSearchesList.appendChild(listItem)
+   })
+}
+
+let recentSearch = getRecentSearches()
+displayRecentSearched(recentSearch)
