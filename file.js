2e88dca23fd3b5a5735dd0fb6ba9314dbba7b163
Jakub Kozera <kozerakuba@gmail.com>
2023-12-01 08:28:22 +0100

    6.4 Nawigacja po adresach

--- file.js
+++ file.js
@@ -1,23 +1,7 @@
-localStorage.setItem("language", "pl")
-localStorage.setItem("theme", "dark")
+document.getElementById("btn1").addEventListener("click", () => {
+  location.href = "contact.html"
+})
 
-const themeColor = localStorage.getItem("theme")
-console.log(themeColor)
-
-localStorage.removeItem("theme")
-
-
-const themeColor2 = localStorage.getItem("theme")
-console.log(themeColor2)
-
-localStorage.clear()
-
-const userSettings = {
-   theme: "dark",
-   lang: "en"
-}
-
-localStorage.setItem("settings", JSON.stringify(userSettings))
-
-const storedSettings = JSON.parse(localStorage.getItem("settings"))
-console.log(storedSettings)
+document.getElementById("btn-back").addEventListener("click", () => {
+  history.back()
+})
