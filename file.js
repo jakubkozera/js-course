a6b30d5c1326eed0bb768203cbc0acaafeaf7883
Jakub Kozera <kozerakuba@gmail.com>
2023-12-06 07:47:23 +0100

    7.5 async i await

--- file.js
+++ file.js
@@ -1,25 +1,21 @@
-window.onload = () => {
+window.onload = async () => {
     const tableBody = document.querySelector("table tbody")
     const apiBaseUrl = "https://users-api-jk.azurewebsites.net"
-    fetch(`${apiBaseUrl}/api/Users`)
-        .then(respone => {
-            console.log(respone)
+    const respone = await fetch(`${apiBaseUrl}/api/Users`)
+    console.log(respone)
+    const data = await respone.json()
+    console.log('data', data)
+    const userRows = data.map(user => {
+        return `<tr>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.address.street}</td>
+        <td>${user.address.city}</td>
+        <td>${user.address.zipCode}</td>
+        </tr>`
+    })
 
-            return respone.json()
-        }).then(data => {
-            console.log('data', data)
-            const userRows = data.map(user => {
-                return `<tr>
-                <td>${user.id}</td>
-                <td>${user.name}</td>
-                <td>${user.email}</td>
-                <td>${user.address.street}</td>
-                <td>${user.address.city}</td>
-                <td>${user.address.zipCode}</td>
-                </tr>`
-            })
+    tableBody.innerHTML = userRows.join("")
 
-            tableBody.innerHTML = userRows.join("")
-
-        })
 }
