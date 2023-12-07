a24446c49c8e136f351096620963e47d82184ac0
Jakub Kozera <kozerakuba@gmail.com>
2023-12-07 07:11:36 +0100

    2.4.1 Metoda flat map

--- file.js
+++ file.js
@@ -1,26 +1,23 @@
-// metoda map
-
-const numbers = [1, 2, 3, 4, 5]
-
-const doubledNumbers = numbers.map((x) =>  x * 2)
-
-console.log(numbers)
-console.log(doubledNumbers)
+// metoda flat map
 
 const people = [
     {
-        name: "Anna", age: 30
+        name: "Anna", age: 30, phoneNumbers: [ "111-222-333", "444-555-666" ]
     },
     {
-        name: "Jan", age: 25
+        name: "Jan", age: 25, phoneNumbers: [ "777-888-999", "000-111-222" ]
     },
     {
-        name: "Ewa", age: 35
+        name: "Ewa", age: 35, phoneNumbers: [ "333-444-555"]
     },
-]
+] 
 
-const names = people
-    .filter(p => p.age >= 30)
-    .map(p => p.name)
+// let allNumbers = []
 
-console.log(names)
+// people.map(person => person.phoneNumbers)
+//         .forEach(phoneNumbers => { allNumbers = allNumbers.concat(phoneNumbers)})
+
+// console.table(allNumbers)
+
+const allNumbers = people.flatMap(person => person.phoneNumbers)
+console.table(allNumbers)
